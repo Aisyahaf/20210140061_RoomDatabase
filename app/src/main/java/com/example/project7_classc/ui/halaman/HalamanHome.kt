d485e33f15d8772db83ae7937d9c53d78e209993
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-05 22:52:38 +0700

    Add Object DestinasiHome

--- app/src/main/java/com/example/project7_classc/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/project7_classc/ui/halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.project7_classc.ui.halaman
 
+import com.example.project7_classc.R
+import com.example.project7_classc.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
