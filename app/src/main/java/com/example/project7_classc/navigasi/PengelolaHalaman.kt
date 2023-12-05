3505e7728cce83b4457bdfb28ba6aae287a5709e
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-05 23:28:03 +0700

    Add Function SiswaApp

--- app/src/main/java/com/example/project7_classc/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project7_classc/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,11 @@
 package com.example.project7_classc.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun  SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
