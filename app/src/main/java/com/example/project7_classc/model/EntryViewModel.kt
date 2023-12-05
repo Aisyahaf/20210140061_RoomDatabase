fd0194632a36c206a6b6c715165d47bced757b74
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-05 21:27:00 +0700

    Add Class EntryViewModel

--- app/src/main/java/com/example/project7_classc/model/EntryViewModel.kt
+++ app/src/main/java/com/example/project7_classc/model/EntryViewModel.kt
@@ -1,2 +1,34 @@
 package com.example.project7_classc.model
 
+import android.app.PictureInPictureUiState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.project7_classc.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+//    Berisi Status  Siswa saat ini
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /*Fungsi untuk memvalidasi input*/
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa(){
+        if (validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
