bf1e320608257a0fb2a8cee1951e7c27368cf245
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-05 22:14:27 +0700

    Add interface DestinasiNavigasi

--- app/src/main/java/com/example/project7_classc/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/project7_classc/navigasi/DestinasiNavigasi.kt
@@ -1,4 +1,13 @@
 package com.example.project7_classc.navigasi
 
 interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yg berisis judul yg akan ditampilkan di layar halaman
+     */
+    val titleRes: Int
 }
