73e60b9994dd995aa79fff30634b1856ba3ae943
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-05 22:09:34 +0700

    Add Object PenyediaViewModel

--- app/src/main/java/com/example/project7_classc/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project7_classc/model/PenyediaViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.project7_classc.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.project7_classc.AplikasiSiswa
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+/***
+ * Fungsi Ekstensi query untuk objek dan mengembalikan sebuah instance
+ */
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
