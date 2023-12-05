ae08ba619d55307885a4bf5f9f3482c931996d5d
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-05 23:12:34 +0700

    Add Function BodyHome

--- app/src/main/java/com/example/project7_classc/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/project7_classc/ui/halaman/HalamanHome.kt
@@ -1,5 +1,6 @@
 package com.example.project7_classc.ui.halaman
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,18 +12,22 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.example.project7_classc.R
 import com.example.project7_classc.model.HomeViewModel
 import com.example.project7_classc.navigasi.DestinasiNavigasi
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.project7_classc.data.Siswa
 import com.example.project7_classc.model.PenyediaViewModel
 
 object DestinasiHome : DestinasiNavigasi{
@@ -69,5 +74,29 @@ fun HomeScreen(
                 .fillMaxSize()
         )
 
+    }
+}
+
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ){
+        if (itemSiswa.isEmpty()){
+            Text(
+                text = stringResource(R.string.deksripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }else{
+            ListSiswa(
+                itemSiswa = itemSiswa,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
     }
 }
