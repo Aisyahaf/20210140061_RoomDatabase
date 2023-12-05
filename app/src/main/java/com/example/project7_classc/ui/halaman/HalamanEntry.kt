4b30d58c44e8169e5a2d9b7292e13fd23693ff94
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-12-05 22:22:16 +0700

    Add object DestinasiEntry

--- app/src/main/java/com/example/project7_classc/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/project7_classc/ui/halaman/HalamanEntry.kt
@@ -1,2 +1,9 @@
 package com.example.project7_classc.ui.halaman
 
+import com.example.project7_classc.R
+import com.example.project7_classc.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
